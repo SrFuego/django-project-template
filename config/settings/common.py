e67344e3aa033ea19edb10810cf4834d41bd4b3f
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-02 07:26:26 -0500

    ADD graphene-django package

--- config/settings/common.py
+++ config/settings/common.py
@@ -36,6 +36,7 @@ DJANGO_APPS = (
 
 THIRD_PARTY_APPS = (
     "pipeline",
+    "graphene_django",
 )
 
 LOCAL_APPS = (
@@ -139,4 +140,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # Pipeline configuration
 PIPELINE = {
     "PIPELINE_ENABLED": True,
+}
+
+# Graphene configuration
+GRAPHENE = {
+    "SCHEMA": "apps.core.schema"
 }
--- config/urls/common.py
+++ config/urls/common.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from graphene_django.views import GraphQLView
+
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
-    url(r"^{{ project_name }}", include("apps.core.urls", namespace="core")),
+    url(r"^graphql", GraphQLView.as_view(graphiql=True)),
 ]
