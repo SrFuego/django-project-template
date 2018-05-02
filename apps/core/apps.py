4332f97dac256ddcc48bc2eb7d411dc3bbf9af08
Manuel Jesús De la Cruz Sotelo <manuel7851@gmail.com>
2018-05-02 05:49:19 -0500

    ADD improve core app admin name to project name

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -14,4 +14,4 @@ from django.apps import AppConfig
 # Configure your app here.
 class CoreConfig(AppConfig):
     name = "apps.core"
-    verbose_name = "{{ project_name | title }} App"
+    verbose_name = "{{ project_name | title }}"
